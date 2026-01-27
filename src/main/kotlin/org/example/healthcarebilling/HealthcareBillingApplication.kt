e2d6d12fb2c3309b594b89cc4ac80a200f4f2471
sagaryadav <sagar619yadav@gmail.com>
2026-01-27 15:21:41 +0530

    initialize project with Gradle and Spring Boot setup

---
+++ src/main/kotlin/org/example/healthcarebilling/HealthcareBillingApplication.kt
@@ -0,0 +1,11 @@
+package org.example.healthcarebilling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealthcareBillingApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealthcareBillingApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/healthcarebilling/HealthcareBillingApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.healthcarebilling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HealthcareBillingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
