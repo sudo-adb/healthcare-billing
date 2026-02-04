26a4e4e6b58d1405e977376e5047ef09106eed36
sagaryadav <sagar619yadav@gmail.com>
2026-02-04 23:53:56 +0530

    add age calculation to Patient model and create corresponding unit test

--- src/main/kotlin/org/example/healthcarebilling/domain/patient/Patient.kt
+++ src/main/kotlin/org/example/healthcarebilling/domain/patient/Patient.kt
@@ -1,10 +1,12 @@
 package org.example.healthcarebilling.domain.patient
 
 import java.time.LocalDate
+import java.time.Period
 
 data class Patient(
     val firstName: String,
     val lastName: String,
     val dateOfBirth: LocalDate
 ) {
+    val age: Int get() = Period.between(dateOfBirth, LocalDate.now()).years
 }
---
+++ src/test/kotlin/org/example/healthcarebilling/domain/patient/PatientTest.kt
@@ -0,0 +1,21 @@
+package org.example.healthcarebilling.domain.patient
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.time.LocalDate
+import java.time.Period
+import kotlin.test.Test
+
+class PatientTest {
+
+    @Test
+    fun `given a patient should calculate age correctly`() {
+        val patient = Patient(
+            firstName = "John",
+            lastName = "Smith",
+            dateOfBirth = LocalDate.of(1990, 6, 15)
+        )
+
+        val expectedAge = Period.between(patient.dateOfBirth, LocalDate.now()).years
+        assertEquals(expectedAge, patient.age)
+    }
+}
